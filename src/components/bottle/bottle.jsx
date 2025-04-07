9deef9655189955a1803777e11bf2572063a3054
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-07 20:25:11 +0600

    added buy now and add to cart button

--- src/components/bottle/bottle.jsx
+++ src/components/bottle/bottle.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './bottle.css'
 
 const Bottle = ({bottle}) => {
-    const {img, name} = bottle;
+    const {img, name, seller, stock, price} = bottle;
     return (
         <div className='product-card bottle'>
             <img src={img} alt="" />
             <h3>Name: {name} </h3>
+            <h4>Brand: {seller}</h4>
+            <h3>${price}</h3>
+            <p>{stock > 0 ? 'In stock' : 'Out of stock'}</p>
+            <div style={{display:'flex', justifyContent:'space-between'}}>
+            <button>Buy now</button>
+            <button>Add to cart</button>
+            </div>
         </div>
     );
 };
